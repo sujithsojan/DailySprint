7505400442b3427d74cdd544ddf1beb2ee54f1e3
Sujith <sujithsojan@gmail.com>
2024-05-15 21:20:59 +0530

    first commit of mobile code

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(156, 39, 176, 1),
+                Color.fromRGBO(3, 169, 244, 1)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello World!'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
